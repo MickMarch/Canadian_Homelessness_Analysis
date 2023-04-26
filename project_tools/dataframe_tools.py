5beb008f394877790fe2d1298608a09c7006f8a9
MickMarch <michael.r.marchand@gmail.com>
2023-04-26 15:58:06 -0400

    updated column tool

--- project_tools/dataframe_tools.py
+++ project_tools/dataframe_tools.py
@@ -1,14 +1,24 @@
 from typing import List
 
+
 def column_name_reformating(df):
     """rename all Pandas columns to a lowercase/underscore format"""
 
+    try:
+        old_columns: List[str] = df.columns
+        new_columns = []
 
-    old_columns: List[str] = df.columns
-    new_columns = []
+        for column in old_columns:
+            new_columns.append(
+                column.lower().replace(" ", "_").replace("(", "").replace(")", "")
+            )
 
-    for column in old_columns:
-        new_columns.append(column.lower().replace(" ", "_").replace("(", "").replace(")", ""))
+        df.columns = new_columns
 
-    df.columns = new_columns
-    return df
+    except Exception as err:
+        print(
+            "-------------------------\n"
+            f"Error: {err}\n"
+            "Unable to convert columns labels\n"
+            "-------------------------"
+        )
